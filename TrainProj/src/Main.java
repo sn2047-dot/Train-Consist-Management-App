fc119c2b8eed167b26499c20b5bf9ad6ff92e292
sn2047-dot <sn2047@srmist.edu.in>
2026-03-25 09:57:00 +0530

    UC1

---
+++ TrainProj/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the train system");
+
+        List<String> trainConsist =  new ArrayList<>();
+
+        System.out.println("Bogie available: " + trainConsist.size());
+        System.out.println("Current Train Consist: " + trainConsist);
+        System.out.println("\nSystem ready for operations...");
+
+
+    }
+}
